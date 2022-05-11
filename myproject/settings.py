2e65b261d998f4237036f50d99f0dc569ff2f501
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-05-11 15:26:05 +0000

    debug to false

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io',
