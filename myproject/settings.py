562ac22af43ed39175aa59fca3bb7a1f4b4b5643
Nick McMillen <nick.mcmillen@student.awesomeinc.org>
2022-05-11 15:47:52 +0000

    debug back to false

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,13 +33,13 @@ SECRET_KEY = env('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = False
-DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io',
 'https://*.herokuapp.com'
 ]
+
 SITE_ID = 1
 
 # Application definition
